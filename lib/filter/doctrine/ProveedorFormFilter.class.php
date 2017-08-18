54ae43253b9fa1ac3a90b7964ad047b07676645f
Javier <javier.legrand@gmail.com>
2017-08-18 10:02:09 -0300

    FIX filtros proveedor backend.

--- lib/filter/doctrine/ProveedorFormFilter.class.php
+++ lib/filter/doctrine/ProveedorFormFilter.class.php
@@ -10,7 +10,34 @@
  */
 class ProveedorFormFilter extends BaseProveedorFormFilter
 {
-  public function configure()
-  {
-  }
+
+    public function configure()
+    {
+        $this->setWidgets(array(
+            'rut_proveedor' => new sfWidgetFormFilterInput(),
+            'razon_social'  => new sfWidgetFormFilterInput(),
+            'telefono'      => new sfWidgetFormFilterInput(),
+            'email'         => new sfWidgetFormFilterInput(),
+        ));
+
+        $this->setValidators(array(
+            'rut_proveedor' => new sfValidatorPass(array('required' => false)),
+            'razon_social'  => new sfValidatorPass(array('required' => false)),
+            'telefono'      => new sfValidatorPass(array('required' => false)),
+            'email'         => new sfValidatorPass(array('required' => false)),
+        ));
+
+        $this->widgetSchema->setNameFormat('proveedor_filters[%s]');
+    }
+
+    public function getFields()
+    {
+        return array(
+            'id_proveedor'  => 'Number',
+            'rut_proveedor' => 'text',
+            'razon_social'  => 'Text',
+            'telefono'      => 'Text',
+            'email'         => 'Text',
+        );
+    }
 }
